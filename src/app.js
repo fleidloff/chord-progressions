2704e3cbe39d6049f63d5e9516d5ae90d9c40ddb
Frederik Leidloff <fleidloff@team.mobile.de>
2018-02-15 19:05:11 +0100

    initial commit of chord progressions library

---
+++ src/app.js
@@ -0,0 +1,19 @@
+var teoria = require('teoria'),
+    teoriaChordProgression = require('teoria-chord-progression');
+
+
+
+function toChordProgression(note = 'c', scale = 'ionian', chords = [2, 5, 1]) {
+	// create teoria.scale object
+	var cMajorScale = teoria.scale(note, scale);
+
+	// construct a diatonic chord progression
+	var twoFiveOne = teoriaChordProgression(cMajorScale, chords);
+
+	// or pass chord length argument to build 7th chords
+	var result = teoriaChordProgression(cMajorScale, chords, 4);
+
+	return result.chords.map(chord => chord.name);
+}
+
+console.log(toChordProgression('c', 'ionian', [2, 5, 1]));
