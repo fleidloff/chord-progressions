0c81d79777f92f6b48c93da7e6be52ee2553f6fa
Frederik Leidloff <fleidloff@team.mobile.de>
2018-02-16 10:10:34 +0100

    improve random method as preparation for random keys and scales

--- src/app.js
+++ src/app.js
@@ -20,43 +20,23 @@ function generateRandomChordProgression(nrChords = 4) {
 	var result = [ 1 ];
 
 	for (var i = 1; i < nrChords; i++) {
-		result.push(nextChord(result[result.length - 1]));
+		result.push(randomItemFromArray(progressions[result[result.length - 1]]));
 	}
 
 	return result;
 }
 
-function nextChord(position = 1) {
-	// todo: decide randomly on position
-	const random = Math.floor(Math.random()*100);
-	var followPosition = 0;
-
-	if (random > 90) {
-		followPosition = 4; 
-	}
-
-	if (random > 80) {
-		followPosition = 3; 
-	}
-
-	if (random > 60) {
-		followPosition = 2; 
-	}
-
-	if (random > 40) {
-		followPosition = 1; 
-	}
-
-	return progressions[position][followPosition];
+function randomItemFromArray(items) {
+	return items[Math.floor(Math.random()*items.length)];
 }
 
 const progressions = {
-	1: [ 4, 5, 6, 2, 3],
-	2: [ 5, 4, 6, 1, 3],
-	3: [ 6, 4, 1, 2, 5],
-	4: [ 5, 1, 2, 3, 6],
-	5: [ 1, 4, 6, 2, 3],
-	6: [ 2, 5, 3, 4, 1]
+	1: [ 4, 4, 4, 4, 5, 5, 6, 6, 2, 3 ],
+	2: [ 5, 5, 5, 5, 4, 4, 6, 6, 1, 3 ],
+	3: [ 6, 6, 6, 6, 4, 4, 1, 2, 5 ],
+	4: [ 5, 5, 5, 5, 1, 1, 2, 2, 3, 6 ],
+	5: [ 1, 1, 1, 1, 1, 4, 4, 6, 6, 2, 3 ],
+	6: [ 2, 2, 2, 2, 5, 5, 3, 3, 4, 1 ]
 }
 
 console.log(toChordProgression('c', 'ionian', generateRandomChordProgression(4)));
