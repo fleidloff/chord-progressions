53a0cf33ad54f4f7d878886842ecacc28b257845
Frederik Leidloff <fleidloff@team.mobile.de>
2018-02-20 11:31:11 +0100

    some cleanup

--- src/app.js
+++ src/app.js
@@ -5,14 +5,9 @@ var teoria = require('teoria'),
 
 function toChordProgression(note = 'c', scale = 'ionian', chords = [2, 5, 1]) {
 	console.log(note, scale, teoria.note(note).scale(scale).simple());
-	// create teoria.scale object
-	var cMajorScale = teoria.scale(note, scale);
-
-	// construct a diatonic chord progression
-	var twoFiveOne = teoriaChordProgression(cMajorScale, chords);
 
 	// or pass chord length argument to build 7th chords
-	var result = teoriaChordProgression(cMajorScale, chords, 4);
+	var result = teoriaChordProgression(teoria.scale(note, scale), chords, 4);
 
 	return result.chords.map(chord => chord.name);
 }
