a0b199d0e31093cdfb8db320e5276aefd14810be
Frederik Leidloff <fleidloff@team.mobile.de>
2018-02-27 10:56:37 +0100

    add hash for js files

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,19 @@
+// todo: babel
 // todo: webpack copy plugin
 // todo: webpack uglify plugin
-// todo: assets plugin + chunkhash + generate html file
+// todo: less / sass for css bundling
+// todo: cssnano
+const HTMLPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: './src/app.js',
+  entry: './src/js/app.js',
   output: {
-    filename: './docs/js/bundle.js'
-  }
+  	path: __dirname + '/docs',
+    filename: 'js/bundle.[hash].js'
+  },
+  plugins: [
+    new CleanWebpackPlugin(['docs']),
+    new HTMLPlugin({ template: 'src/index.html' })
+  ]
 };
