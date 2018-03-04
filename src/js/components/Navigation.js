e15fa26c8e7c2e8777534499f972396b5bc78542
Frederik Leidloff <fleidloff@team.mobile.de>
2018-03-04 09:15:42 +0100

    only show chord for random when random selected

--- src/js/components/Navigation.js
+++ src/js/components/Navigation.js
@@ -9,13 +9,13 @@ export default withConsumer(({ state, actions }) => (
 			<div className="pure-g Navigation">
 			    <div className="pure-u-1 pure-u-sm-2-5">
 			  		<select onChange={actions.onChangeSelectedNote} id="note" value={state.selected.note}>
-			        	{[ "random", ...notes ].map((note, idx) => <option key={idx} value={note}>{note}{state.note === "random" && ` (${state.note})`}</option>)}
+			        	{[ "random", ...notes ].map((note, idx) => <option key={idx} value={note}>{note}{note === "random" && state.selected.note === "random" ? ` (${state.note})` : ""}</option>)}
 			        </select>
 			    </div>
 			    <div className="pure-u-1 pure-u-sm-1-5"><br /></div>
 			    <div className="pure-u-1 pure-u-sm-2-5">
 			        <select onChange={actions.onChangeSelectedScale} id="scale" value={state.selected.scale}>
-			        	{[ "random", ...scales ].map((scale, idx) => <option key={idx} value={scale}>{scale}{state.scale === "random" && ` (${state.scale})`}</option>)}
+			        	{[ "random", ...scales ].map((scale, idx) => <option key={idx} value={scale}>{scale}{scale === "random" && state.selected.scale === "random" ? ` (${state.scale})` : ""}</option>)}
 			        </select>
 		        </div>
 			</div>
