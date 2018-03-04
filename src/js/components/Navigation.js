cdca486ef0d4b4dece8cc33569ede66268643e3e
Frederik Leidloff <fleidloff@team.mobile.de>
2018-03-04 08:45:41 +0100

    better styling

--- src/js/components/Navigation.js
+++ src/js/components/Navigation.js
@@ -7,22 +7,20 @@ export default withConsumer(({ state, actions }) => (
 	<div>
 		<form class="pure-form">
 			<div class="pure-g">
-			    <div class="pure-u-1 pure-u-sm-1-3">
-			    	<label for="note">Root</label><br />
+			    <div class="pure-u-1 pure-u-md-1-2">
 			  		<select onChange={actions.onChangeSelectedNote} id="note" value={state.selected.note}>
 			        	{[ "random", ...notes ].map((note, idx) => <option key={idx} value={note}>{note}</option>)}
 			        </select>
 			    </div>
-			    <div class="pure-u-1 pure-u-sm-1-3">
-				    <label for="scale">Scale</label><br />
+			    <div class="pure-u-1 pure-u-md-1-2">
 			        <select onChange={actions.onChangeSelectedScale} id="scale" value={state.selected.scale}>
 			        	{[ "random", ...scales ].map((scale, idx) => <option key={idx} value={scale}>{scale}</option>)}
 			        </select>
 		        </div>
-			    <div class="pure-u-1 pure-u-sm-1-3">
-			    	<br />
-	 				<button onClick={actions.random} className="pure-button">new chords</button>
-			    </div>
+
+		    	<br />
+ 				<button onClick={actions.random} className="pure-button">new chords</button>
+
 			</div>
         </form>
 	</div>
